65101174e346397c192e84c97c138345bd785abc
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-03-01 20:45:12 +0100

    fix link length

--- src/main/java/ch/ethz/matsim/baseline_scenario/zurich/RunZurichScenario.java
+++ src/main/java/ch/ethz/matsim/baseline_scenario/zurich/RunZurichScenario.java
@@ -1,9 +1,9 @@
 package ch.ethz.matsim.baseline_scenario.zurich;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
-import org.matsim.core.config.groups.StrategyConfigGroup.StrategySettings;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
 
@@ -18,6 +18,11 @@ public class RunZurichScenario {
 		config.qsim().setNumberOfThreads(Integer.parseInt(args[2]));
 
 		Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		for (Link link : scenario.getNetwork().getLinks().values()) {
+			link.setLength(Math.max(10.0, link.getLength()));
+		}
+
 		Controler controler = new Controler(scenario);
 
 		controler.addOverridingModule(new BaselineModule());
