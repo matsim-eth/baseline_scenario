33f92637d259cbcd86e1c396160bb121fc0e2708
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-06-24 17:00:40 +0200

    Make useable in scenario

--- src/main/java/ch/ethz/matsim/baseline_scenario/zurich/router/modules/CarRoutingModule.java
+++ src/main/java/ch/ethz/matsim/baseline_scenario/zurich/router/modules/CarRoutingModule.java
@@ -1,28 +1,41 @@
 package ch.ethz.matsim.baseline_scenario.zurich.router.modules;
 
+import java.util.Optional;
+
 import org.matsim.api.core.v01.network.Network;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Key;
 import com.google.inject.Provides;
 import com.google.inject.multibindings.MapBinder;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 
 import ch.ethz.matsim.baseline_scenario.zurich.router.trip.CarTripRouter;
 import ch.ethz.matsim.baseline_scenario.zurich.router.trip.TripRouter;
 
 public class CarRoutingModule extends AbstractModule {
-	final private Network roadNetwork;
+	final private Optional<Network> roadNetwork;
 
 	public CarRoutingModule(Network roadNetwork) {
-		this.roadNetwork = roadNetwork;
+		this.roadNetwork = Optional.of(roadNetwork);
+	}
+
+	public CarRoutingModule() {
+		this.roadNetwork = Optional.empty();
 	}
 
 	@Override
 	protected void configure() {
+		if (roadNetwork.isPresent()) {
+			bind(Key.get(Network.class, Names.named("road"))).toInstance(roadNetwork.get());
+		}
+
 		MapBinder.newMapBinder(binder(), String.class, TripRouter.class).addBinding("car").to(CarTripRouter.class);
 	}
 
 	@Provides
-	public CarTripRouter provideCarTripRouter() {
+	public CarTripRouter provideCarTripRouter(@Named("road") Network roadNetwork) {
 		return new CarTripRouter(roadNetwork);
 	}
 }
--- src/main/java/ch/ethz/matsim/baseline_scenario/zurich/router/modules/PublicTransitRoutingModule.java
+++ src/main/java/ch/ethz/matsim/baseline_scenario/zurich/router/modules/PublicTransitRoutingModule.java
@@ -1,6 +1,9 @@
 package ch.ethz.matsim.baseline_scenario.zurich.router.modules;
 
+import java.util.Optional;
+
 import org.matsim.api.core.v01.network.Network;
+import org.matsim.core.config.Config;
 import org.matsim.core.config.groups.PlansCalcRouteConfigGroup;
 import org.matsim.core.config.groups.PlansCalcRouteConfigGroup.ModeRoutingParams;
 import org.matsim.pt.config.TransitRouterConfigGroup;
@@ -11,6 +14,7 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.multibindings.MapBinder;
+import com.google.inject.name.Named;
 
 import ch.ethz.matsim.baseline_scenario.transit.connection.DefaultTransitConnectionFinder;
 import ch.ethz.matsim.baseline_scenario.transit.connection.TransitConnectionFinder;
@@ -22,22 +26,46 @@ import ch.ethz.matsim.baseline_scenario.zurich.router.trip.PublicTransitTripRout
 import ch.ethz.matsim.baseline_scenario.zurich.router.trip.TripRouter;
 
 public class PublicTransitRoutingModule extends AbstractModule {
-	final private Network network;
-	final private TransitSchedule transitSchedule;
-	final private ModeRoutingParams transitWalkParams;
+	final private Optional<Network> network;
+	final private Optional<TransitSchedule> transitSchedule;
+	final private Optional<ModeRoutingParams> transitWalkParams;
 
 	public PublicTransitRoutingModule(Network network, TransitSchedule transitSchedule,
 			ModeRoutingParams transitWalkParams) {
-		this.network = network;
-		this.transitSchedule = transitSchedule;
-		this.transitWalkParams = transitWalkParams;
+		this.network = Optional.of(network);
+		this.transitSchedule = Optional.of(transitSchedule);
+		this.transitWalkParams = Optional.of(transitWalkParams);
+	}
+
+	public PublicTransitRoutingModule() {
+		this.network = Optional.empty();
+		this.transitSchedule = Optional.empty();
+		this.transitWalkParams = Optional.empty();
 	}
 
 	@Override
 	protected void configure() {
 		MapBinder.newMapBinder(binder(), String.class, TripRouter.class).addBinding("pt")
 				.to(PublicTransitTripRouter.class);
-		bind(TransitSchedule.class).toInstance(transitSchedule);
+
+		if (network.isPresent()) {
+			bind(Network.class).toInstance(network.get());
+		}
+
+		if (transitSchedule.isPresent()) {
+			bind(TransitSchedule.class).toInstance(transitSchedule.get());
+		}
+	}
+
+	@Provides
+	@Singleton
+	@Named("transit_walk")
+	public ModeRoutingParams provideTransitWalkParams(Config config) {
+		if (transitWalkParams.isPresent()) {
+			return transitWalkParams.get();
+		} else {
+			return config.plansCalcRoute().getModeRoutingParams().get("walk");
+		}
 	}
 
 	@Provides
@@ -55,7 +83,7 @@ public class PublicTransitRoutingModule extends AbstractModule {
 	@Provides
 	public EnrichedTransitRouter provideEnrichedTransitRouter(TransitRouter delegate,
 			TransitConnectionFinder connectionFinder, PlansCalcRouteConfigGroup routeConfig,
-			TransitRouterConfigGroup transitConfig) {
+			TransitRouterConfigGroup transitConfig, TransitSchedule transitSchedule, Network network) {
 		double beelineDistanceFactor = routeConfig.getBeelineDistanceFactors().get("walk");
 		double additionalTransferTime = transitConfig.getAdditionalTransferTime();
 
@@ -65,7 +93,8 @@ public class PublicTransitRoutingModule extends AbstractModule {
 
 	@Provides
 	public PublicTransitTripRouter providePublicTransitTripRouter(DepartureFinder departureFinder,
-			EnrichedTransitRouter transitRouter) {
+			EnrichedTransitRouter transitRouter, TransitSchedule transitSchedule, Network network,
+			@Named("transit_walk") ModeRoutingParams transitWalkParams) {
 		return new PublicTransitTripRouter(network, transitSchedule, transitRouter, transitWalkParams);
 	}
 }
