153db823404af6ffa0825dd7dc97baf9d71b0408
Sebastian Hörl <hoerl.sebastian@gmail.com>
2018-10-10 09:06:45 +0200

    add downsampling utility

---
+++ src/main/java/ch/ethz/matsim/baseline_scenario/preparation/Downsample.java
@@ -0,0 +1,44 @@
+package ch.ethz.matsim.baseline_scenario.preparation;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.population.io.PopulationReader;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class Downsample {
+	final private double probability;
+	final private Random random;
+
+	public Downsample(double fraction, Random random) {
+		this.probability = fraction;
+		this.random = random;
+	}
+
+	public void run(Population population) {
+		Iterator<? extends Person> personIterator = population.getPersons().values().iterator();
+
+		while (personIterator.hasNext()) {
+			personIterator.next();
+
+			if (random.nextDouble() >= probability) {
+				personIterator.remove();
+			}
+		}
+	}
+
+	static public void main(String[] args) {
+		Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		new PopulationReader(scenario).readFile(args[0]);
+
+		double probability = Double.parseDouble(args[1]);
+		new Downsample(probability, new Random()).run(scenario.getPopulation());
+
+		new PopulationWriter(scenario.getPopulation()).write(args[2]);
+	}
+}
